20d653192d7ef638c73ee4a2fe42151235660d56
black alfa <92008277+yasasdileepa@users.noreply.github.com>
2021-11-18 14:38:38 +0530

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2021 AmdA.
+/* Copyright (C) 2021 yasasdileepa .
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-Amdibell - AmdA
+Maraya - yasasdileepa 
 */
 
 const Asena = require('../events');
